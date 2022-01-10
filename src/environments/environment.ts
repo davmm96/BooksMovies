10b8e9e27885ed8f80580532dede0f8bba15276c
davmm96 <dmelero96@gmail.com>
2022-01-10 12:17:34 +0100

    Esqueleto basico

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '7IF0mcJhOxZAHIWbBGqA4NT6dUXkRxFZ',
+  apiUrl: 'https://api.nytimes.com/'
 };
 
 /*
