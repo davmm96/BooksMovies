5b92b95d778adfe1d188988898e195675b1f5ba5
davmm96 <dmelero96@gmail.com>
2022-01-12 00:03:27 +0100

    Menu share movie Favoritos boton movie

--- src/app/components/movie/movie.component.ts
+++ src/app/components/movie/movie.component.ts
@@ -4,6 +4,7 @@ import { Movie } from 'src/app/interfaces/interfacesMovies';
 import { ActionSheetController } from '@ionic/angular';
 
 import { FavoritesService } from 'src/app/services/favorites.service';
+import { SocialSharing } from '@awesome-cordova-plugins/social-sharing/ngx';
 
 @Component({
   selector: 'app-movie',
@@ -15,45 +16,76 @@ export class MovieComponent implements OnInit {
   @Input() movie: Movie;
   @Input() isFavorite: boolean;
 
-  constructor(private actionSheetController: ActionSheetController, private favoritesService: FavoritesService) { }
+  constructor(private actionSheetController: ActionSheetController, private favoritesService: FavoritesService, private socialSharing: SocialSharing) { }
 
   ngOnInit() {}
 
-  async mostrarMenu(){
-
-    let guardarBorrarFavorito;
-
-    if(this.isFavorite)
-    {
-      guardarBorrarFavorito = {
-        text: 'Remove from favoritos',
-          icon: 'trash',
+  async mostrarMenu()
+  {
+      const actionSheet = await this.actionSheetController.create({
+        header: 'Share',
+        buttons: [{
+            text: 'Twitter',
+            icon: 'logo-twitter',
+            handler: () => {
+              this.shareTwitter();
+            }
+        },
+        {
+          text: 'Facebook',
+          icon: 'logo-facebook',
           handler: () => {
-            this.favoritesService.removeFavMovie(this.movie);
+            this.shareFacebook();
           }
-      }
-    }
-    else
-    {
-      guardarBorrarFavorito = {
-        text: 'Add to favorites',
-          icon: 'heart',
+        },
+        {
+          text: 'Instagram',
+          icon: 'logo-instagram',
           handler: () => {
-            this.favoritesService.addFavMovie(this.movie);
+            this.shareInstagram();
           }
-      }
-    }
-    const actionSheet = await this.actionSheetController.create({
-      buttons: [guardarBorrarFavorito, {
-        text: 'Cancel',
-        icon: 'close',
-        role: 'cancel',
-        handler: () => {
-        }
-      }]
-    });
+        },
+        {
+          text: 'Cancel',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+          }
+        }]
+      });
 
-    await actionSheet.present();
+      await actionSheet.present();
+    }
+
+  addFavorite()
+  {
+    this.favoritesService.addFavMovie(this.movie);
+  }
+
+  removeFavorite()
+  {
+    this.favoritesService.removeFavMovie(this.movie);
+  }
+
+  shareTwitter()
+  {
+    this.socialSharing.shareViaTwitter(this.movie.display_title, this.movie.multimedia.src).then(() => {
+      // Success!
+    })
+  }
+
+  shareFacebook()
+  {
+    this.socialSharing.shareViaFacebookWithPasteMessageHint(this.movie.display_title, this.movie.multimedia.src).then(() => {
+      // Success!
+    })
+  }
+
+  shareInstagram()
+  {
+    this.socialSharing.shareViaInstagram(this.movie.display_title, this.movie.multimedia.src).then(() => {
+      // Success!
+    })
   }
 
 }
