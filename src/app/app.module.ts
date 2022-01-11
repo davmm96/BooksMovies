c91b59d5f967011472c2d844125a4b36d721666b
davmm96 <dmelero96@gmail.com>
2022-01-11 23:22:11 +0100

    Social sharing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,13 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SocialSharing } from '@awesome-cordova-plugins/social-sharing/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },SocialSharing],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/components/book/book.component.ts
+++ src/app/components/book/book.component.ts
@@ -4,6 +4,7 @@ import { Book } from 'src/app/interfaces/interfacesBookAll';
 import { ActionSheetController } from '@ionic/angular';
 
 import { FavoritesService } from 'src/app/services/favorites.service';
+import { SocialSharing } from '@awesome-cordova-plugins/social-sharing/ngx';
 
 @Component({
   selector: 'app-book',
@@ -15,37 +16,36 @@ export class BookComponent implements OnInit {
   @Input() book: Book;
   @Input() isFavorite: boolean;
 
-  constructor(private actionSheetController: ActionSheetController, private favoritesService: FavoritesService) { }
+  constructor(private actionSheetController: ActionSheetController, private favoritesService: FavoritesService, private socialSharing: SocialSharing) { }
 
   ngOnInit() {}
 
-  async mostrarMenu(){
-
-    let guardarBorrarFavorito;
-
-    if(this.isFavorite)
-    {
-      guardarBorrarFavorito = {
-        text: 'Remove from favorites',
-          icon: 'trash',
-          handler: () => {
-            this.favoritesService.removeFavBook(this.book);
-          }
-      }
-    }
-    else
-    {
-      guardarBorrarFavorito = {
-        text: 'Add to favorites',
-          icon: 'heart',
-          handler: () => {
-            this.favoritesService.addFavBook(this.book);
-          }
-      }
-    }
-
+  async mostrarMenu()
+  {
       const actionSheet = await this.actionSheetController.create({
-        buttons: [guardarBorrarFavorito, {
+        header: 'Share',
+        buttons: [{
+            text: 'Twitter',
+            icon: 'logo-twitter',
+            handler: () => {
+              this.shareTwitter();
+            }
+        },
+        {
+          text: 'Facebook',
+          icon: 'logo-facebook',
+          handler: () => {
+            this.shareFacebook();
+          }
+        },
+        {
+          text: 'Instagram',
+          icon: 'logo-instagram',
+          handler: () => {
+            this.shareInstagram();
+          }
+        },
+        {
           text: 'Cancel',
           icon: 'close',
           role: 'cancel',
@@ -55,6 +55,37 @@ export class BookComponent implements OnInit {
       });
 
       await actionSheet.present();
+    }
+
+    addFavorite()
+    {
+      this.favoritesService.addFavBook(this.book);
+    }
+
+    removeFavorite()
+    {
+      this.favoritesService.removeFavBook(this.book);
+    }
+
+    shareTwitter()
+    {
+      this.socialSharing.shareViaTwitter(this.book.title, this.book.book_image).then(() => {
+        // Success!
+      })
+    }
+
+    shareFacebook()
+    {
+      this.socialSharing.shareViaFacebookWithPasteMessageHint(this.book.title, this.book.book_image).then(() => {
+        // Success!
+      })
+    }
+
+    shareInstagram()
+    {
+      this.socialSharing.shareViaInstagram(this.book.title, this.book.book_image).then(() => {
+        // Success!
+      })
     }
 
 }
